32e0589cbfab20bb8cb5e5c91bbdf61af7335a3c
Ivan The Warlock <jedi2great@proton.me>
2023-08-13 18:39:40 -0500

    Adds junk js script

---
+++ js_scripts/script.js
@@ -0,0 +1,32 @@
+
+
+function getMilk(money) {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    
+    console.log("Buy " + calcBottles(money/1.5) + " bottles of milk.");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+    return money % 1.5;
+  }
+  function calcBottles(startingMoney, costPerBottle) {
+    var numberOfBottles = Math.floor(startingMoney/costPerBottle);
+    return numberOfBottles;
+  }
+  
+  getMilk(6);
+  
