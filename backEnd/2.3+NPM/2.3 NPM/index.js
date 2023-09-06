bde75297e492133cd025c999b72d2ea4c593e8ef
LuxExpanded <100960075+LuxExpanded@users.noreply.github.com>
2023-09-06 15:19:38 -0500

    reorganized directory folders

---
+++ backEnd/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`)
+
+import superheroes from "superheroes";
+
+var superHeroes = superheroes.random();
+
+console.log(`I am ${superHeroes}!!`)
